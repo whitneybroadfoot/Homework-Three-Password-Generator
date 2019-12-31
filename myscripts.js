d97b1555369801eaf4e0c15405cd69234f489444
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2019-12-31 13:28:52 -0700

    going cross eyed

--- myscripts.js
+++ myscripts.js
@@ -6,8 +6,41 @@ var specialCharacters = ["&", "$", "@", "%", "!"];
 console.log(lowerCase[0]); //this outputs a in the console, good 
 console.log(specialCharacters[2]); //this outputs @ in the console, good
 
-//for loop to go through variables, but what do you need to make index greater or less than? 8 and 128... 
-function generatePassword () {
-   
-}
+//********************************************************************************* */
 
+
+document.getElementById("generate").onclick = function(){
+    alert("Some questions about your password");
+  }
+
+
+//alerts that will prompt the user to stay within parameters 
+function passwordQuestions() {    
+    var passwordLength = prompt("How long does your password need to be?");
+    console.log(passwordLength);
+
+    if ((passwordLength > 128) || (passwordLength < 8) || isNaN(passwordLength)) { 
+        alert("Password must be between 8 - 128");
+        return;
+    } else {
+        var specialChar = confirm("Click OK to include special characters");
+        console.log(specialChar);
+
+        var upperCase = confirm("Click OK to include upper case letters");
+        console.log(upperCase);
+
+        var lowerCase = confirm("Click OK to include lower case letters");
+        console.log(lowerCase);
+
+        var numberPass = confirm("Click OK to include numbers");
+        console.log(numberPass);
+    }      
+
+
+//possible password values
+let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnop0123456789!@#$%^&*()/?"
+
+let password = "";
+
+//create for loop to choose password
+for (var= 0; i<= )
