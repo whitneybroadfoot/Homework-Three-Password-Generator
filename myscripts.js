4fd7b12e4a2d9ed25ceab06cac01f1080bb34d0d
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2019-12-31 12:05:52 -0700

    alerts for user

--- myscripts.js
+++ myscripts.js
@@ -6,7 +6,29 @@ var specialCharacters = ["&", "$", "@", "%", "!"];
 console.log(lowerCase[0]); //this outputs a in the console, good 
 console.log(specialCharacters[2]); //this outputs @ in the console, good
 
-
-function () {
-    
+//for loop, but what do you need to make index greater or less than? 8 and 128... 
+function generatePassword () {
+   
 }
+
+//alerts that will prompt the user to stay within parameters 
+function passwordQuestions() {    
+    var passwordLength = prompt("How long does your password need to be?");
+    console.log(passwordLength);
+
+    if ((passwordLength > 128) || (passwordLength < 8) || isNaN(passwordLength)) { 
+        alert("Password must be between 8 - 128");
+        return;
+    } else {
+        var specialChar = confirm("Click OK to include special characters");
+        console.log(specialChar);
+            
+        var upperCase = confirm("Click OK to include upper case letters");
+        console.log(upperCase);
+        
+        var lowerCase = confirm("Click OK to include lower case letters");
+        console.log(lowerCase);
+        
+        var numberPass = confirm("Click OK to include numbers");
+        console.log(numberPass);
+    }     
