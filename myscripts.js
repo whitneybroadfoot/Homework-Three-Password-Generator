798279a979f4c4562e64d783b293e42c7721d0a0
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2019-12-31 12:53:55 -0700

    take out alerts, didn't like them

--- myscripts.js
+++ myscripts.js
@@ -11,25 +11,3 @@ function generatePassword () {
    
 }
 
-
-//alerts that will prompt the user to stay within parameters 
-function passwordQuestions() {    
-    var passwordLength = prompt("How long does your password need to be?");
-    console.log(passwordLength);
-
-    if ((passwordLength > 128) || (passwordLength < 8) || isNaN(passwordLength)) { 
-        alert("Password must be between 8 - 128");
-        return;
-    } else {
-        var specialChar = confirm("Click OK to include special characters");
-        console.log(specialChar);
-            
-        var upperCase = confirm("Click OK to include upper case letters");
-        console.log(upperCase);
-        
-        var lowerCase = confirm("Click OK to include lower case letters");
-        console.log(lowerCase);
-        
-        var numberPass = confirm("Click OK to include numbers");
-        console.log(numberPass);
-    }     
