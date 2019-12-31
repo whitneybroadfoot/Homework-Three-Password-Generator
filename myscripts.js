a9514b003a4f541966052afeb3d9e28391f6b85e
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2019-12-31 11:11:55 -0700

    testing changes between computer and github

--- myscripts.js
+++ myscripts.js
@@ -7,13 +7,6 @@ console.log(lowerCase[0]); //this outputs a in the console, good
 console.log(specialCharacters[2]); //this outputs @ in the console, good
 
 
-
-// function passwordFunction(lowerCase, upperCase, numbers, specialCharacters) {
-//     return lowerCase + upperCase + numbers + specialCharacters;
-//   }
-
-// console.log(passwordFunction);
-
-function generate(){
+function () {
     
 }
