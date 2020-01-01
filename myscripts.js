518a1334189c8343b878172b6ef40ab1c6c476a6
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-01-01 14:42:04 -0700

    variables

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,13 @@
+//password variable options 
+
+var lowerCase = ["a", "b", "c", "d", "e"];
+var upperCase = ["A", "B", "C", "D", "E"];
+var numbers = ["1", "2", "3", "4", "5",];
+var specialCharacters = ["&", "$", "@", "%", "!"];
+
 //on click event for the password button to generate alerts
 document.getElementById("generate").onclick = function(){
-    alert("Some questions about your password");
+    passwordQuestions()
   }
 
 //alerts that will prompt the user to stay within parameters 
@@ -12,25 +19,34 @@ function passwordQuestions() {
         alert("Password must be between 8 - 128");
         return;
     } else {
+        var passOptions = []
+
         var specialChar = confirm("Click OK to include special characters");
-        console.log(specialChar);
+            if(specialChar){
+                passOptions = passOptions.concat(specialCharacters)
+            }
+            console.log(passOptions);
 
-        var upperCase = confirm("Click OK to include upper case letters");
-        console.log(upperCase);
+        var upperCaseLetters = confirm("Click OK to include upper case letters");
+            if(upperCaseLetters){
+                passOptions = passOptions.concat(upperCase)
+            }
+            console.log(passOptions);
 
-        var lowerCase = confirm("Click OK to include lower case letters");
-        console.log(lowerCase);
+        var lowerCaseLetters = confirm("Click OK to include lower case letters");
+            if(lowerCaseLetters){
+                passOptions = passOptions.concat(lowerCase)
+            }
+            console.log(passOptions);
+        
 
         var numberPass = confirm("Click OK to include numbers");
-        console.log(numberPass);
-    }      
-
-//once cycled through alerts, possible password values
-
-var lowerCase = ["a", "b", "c", "d", "e"];
-var upperCase = ["A", "B", "C", "d", "e"];
-var numbers = ["1", "2", "3", "4", "5",];
-var specialCharacters = ["&", "$", "@", "%", "!"];
+            if(numberPass){
+                passOptions = passOptions.concat(numbers)
+            }
+            console.log(passOptions);    
+    } 
+}
 
 //create for loop to choose password
-for (var = 0; i<= )
+// for (var = 0; i<= )
